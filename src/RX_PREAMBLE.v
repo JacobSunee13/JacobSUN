300e342b09a2ec0b51bb580fa3ba5c295f0c982f
JacobSunee13 <jacob0226.ee13@nycu.edu.tw>
2025-08-22 15:27:36 +0800

    Copy RX_PREAMBLE to repo

---
+++ src/RX_PREAMBLE.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/08/22 11:32:32
+// Design Name: 
+// Module Name: RX_PREAMBLE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RX_PREAMBLE(
+
+    );
+endmodule
